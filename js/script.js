eabfc6b33ad1e9e356d2920f3f2a780ab087535a
mglazarte <maurolazarte75@gmail.com>
2024-09-01 18:36:48 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function areaCirculo(radio) {
+    let pi = 3.14159265359
+    let area = pi * (radio*radio)
+    console.log("Si el radio del círculo es " + radio + " su area es " + area)
+}
+
+areaCirculo(2)
+areaCirculo(4)
+areaCirculo(12)
+areaCirculo(20)
+areaCirculo(40)
